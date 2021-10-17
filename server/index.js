ae63954f03b61ad9b835e117b7054a29e7bf7342
Chris Tijerina <crtijerina1988@gmail.com>
2021-10-17 18:14:44 -0500

    this is is it it

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req,res)=> {
+res.send('Bookish API, Hello!')
+});
+
 app.get('/', (req, res) => {
     req.setEncoding('Bookish');
 });
@@ -33,7 +37,7 @@ app.get('/', (req, res) => {
 
 //const CONNECTION_URL = 'mongodb+srv://Chris:12345%21%40%23%24%25@cluster0.7brun.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`)))
